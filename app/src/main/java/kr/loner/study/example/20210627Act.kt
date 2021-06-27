2aefadba2d71ddee270cc9682b59fb42bcacaaaf
LonerStayle <dlwlstjq410@gmail.com>
2021-06-27 19:09:51 +0900

    first commit

---
+++ app/src/main/java/kr/loner/study/example/20210627Act.kt
@@ -0,0 +1,14 @@
+package kr.loner.study.example
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kr.loner.study.R
+
+class Loner20210627Act: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.example_loner_20210627act)
+        /**Test Code*/
+        // textView.text = "hello World"
+    }
+}
